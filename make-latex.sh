18711769a2d5c4f9a83e4a722eb029a64ba05258
AimiliosPavlidis2001 <p19pavl@ionio.gr>
2023-01-07 20:48:19 +0200

    add changes for paradoteo11

--- make-latex.sh
+++ make-latex.sh
@@ -16,7 +16,7 @@ for filename in text/ch*.txt; do
   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=myluafilter.lua --to markdown |pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
-pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/03epi.tex
+pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/03epi.tex
 
 for filename in text/apx*.txt; do
   [ -e "$filename" ] || continue
